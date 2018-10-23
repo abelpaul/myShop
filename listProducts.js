ad93e7318ca4b64b046ea8b20032b045ea1e7d0f
Abel George <abel.paul.george@gmail.com>
2018-10-23 10:18:16 -0400

    added loop

--- listProducts.js
+++ listProducts.js
@@ -1,10 +1,7 @@
 var faker = require("faker");
 
 
-var product = faker.commerce.productName();
-var price = faker.commerce.price();
-
 for(let i = 0; i<10; i++){
-    console.log(product);
-    console.log(price);
+    console.log(faker.commerce.productName() + " - $" + faker.commerce.price() );
+    
 }
