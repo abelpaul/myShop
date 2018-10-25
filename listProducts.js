c51192d625893cb26f9249c9fb997e9a9729c057
Abel George <abel.paul.george@gmail.com>
2018-10-24 23:38:00 -0400

    added header

--- listProducts.js
+++ listProducts.js
@@ -1,5 +1,7 @@
 var faker = require("faker");
-
+console.log("----------------------")
+console.log("WELCOME TO ABEL'S SHOP")
+console.log("----------------------")
 
 for(let i = 0; i<10; i++){
     console.log(faker.commerce.productName() + " - $" + faker.commerce.price() );
